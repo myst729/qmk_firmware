447aaf5d4727719b21add50d79776201472ac371
Leo Deng <myst.dg@gmail.com>
2020-08-25 10:50:29 +0800

    xtrm firmware

---
+++ keyboards/dg/xtrm/config.h
@@ -0,0 +1,48 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xDE29
+#define PRODUCT_ID      0x7003
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Leo Deng
+#define PRODUCT         Xtrm
+#define DESCRIPTION     Extremely borderless 40% keyboard with encoder
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { D2, D3, F1, B7 }
+#define MATRIX_COL_PINS { F0, D5, F4, F5, F6, F7, C7, C6, B6, B5, B4, D4 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+  keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+/* prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+#define ENCODERS_PAD_A { D7 }
+#define ENCODERS_PAD_B { D6 }
+
+#define TAP_CODE_DELAY 100
+
+#endif
