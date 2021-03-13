3e25f174a6cc751ad926f4a7e879a7a6f95d82bd
Leo Deng <myst.dg@gmail.com>
2021-03-13 19:39:23 +0800

    enjoy: advanced fix

--- keyboards/dg/enjoy/keymaps/advanced/keymap.c
+++ keyboards/dg/enjoy/keymaps/advanced/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     KC_LGUI, KC_LBRC, KC_RBRC, KC_BSLS, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS,          KC_INS,
-    KC_CAPS,          KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END,  KC_PSCR,
+    KC_CAPS,          KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END,  KC_TRNS,
     KC_VOLD, KC_TRNS,          KC_VOLU,          KC_MUTE,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
              KC_UP,
