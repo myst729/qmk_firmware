a6c59446d3b1ffdfb50731ad3cb1ed301c36f0c0
Leo Deng <myst.dg@gmail.com>
2022-06-25 11:53:03 +0800

    djam

---
+++ keyboards/dg/djam/v2/config.h
@@ -0,0 +1,15 @@
+/* STM32F401CCU6 */
+
+/* Device */
+#define DEVICE_VER 0x0002
+
+/* Matrix */
+#define MATRIX_ROW_PINS { B3, B4, B5 }
+#define MATRIX_COL_PINS { B1, B0, A7, A6, A5, A4, A3, A2 }
+// { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A15 }, { B0, B1, B3, B4, B5, B6, B7, B8, B9, B10, B12, B13, B14, B15 }, { C13 }
+
+/* Backlight */
+#define BACKLIGHT_PIN B13
+
+/* RGB Matrix */
+#define RGB_DI_PIN A0
---
+++ keyboards/dg/djam/v3/config.h
@@ -0,0 +1,15 @@
+/* STM32F401CCU6 top SMT */
+
+/* Device */
+#define DEVICE_VER 0x0003
+
+/* Matrix */
+#define MATRIX_ROW_PINS { A6, A7, A1 }
+#define MATRIX_COL_PINS { B3, B4, B5, A2, A5, A4, A3, B1 }
+// { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A15 }, { B0, B1, B3, B4, B5, B6, B7, B8, B9, B10, B12, B13, B14, B15 }, { C13 }
+
+/* Backlight */
+#define BACKLIGHT_PIN B13
+
+/* RGB Matrix */
+#define RGB_DI_PIN B0
