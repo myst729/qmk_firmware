d3f87e436c4fa8c0933ce7d316efb0c57ac2063a
Leo Deng <myst.dg@gmail.com>
2022-06-25 11:53:03 +0800

    djam

---
+++ keyboards/dg/djam/v1/config.h
@@ -0,0 +1,12 @@
+/* ATMEGA32U4 */
+
+/* Matrix */
+#define MATRIX_ROW_PINS { F0, F1, F4 }
+#define MATRIX_COL_PINS { C7, C6, B6, B5, B4, D7, D6, D4 }
+// { B0, B1, B2, B3, B6, B5, B4, B7 }, { C6, C7 }, { D0, D1, D2, D3, D4, D5, D6, D7 }, { E6 }, { F0, F1, F4, F5, F6, F7 }
+
+/* Backlight */
+#define BACKLIGHT_PIN B7
+
+/* RGB Matrix */
+#define RGB_DI_PIN D5
