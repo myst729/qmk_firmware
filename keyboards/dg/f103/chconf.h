211dda483b18a63154b91b9887a3904f9638606c
Leo Deng <myst.dg@gmail.com>
2022-06-25 11:38:04 +0800

    f103

---
+++ keyboards/dg/f103/chconf.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define CH_CFG_ST_TIMEDELTA 0
+#define CH_CFG_USE_REGISTRY TRUE
+#define CH_CFG_USE_WAITEXIT TRUE
+#define CH_CFG_USE_CONDVARS TRUE
+#define CH_CFG_USE_CONDVARS_TIMEOUT FALSE
+#define CH_CFG_USE_MESSAGES TRUE
+#define CH_CFG_USE_MAILBOXES TRUE
+#define CH_CFG_USE_HEAP TRUE
+
+#include_next <chconf.h>
