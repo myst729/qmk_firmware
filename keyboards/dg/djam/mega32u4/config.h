ef5c27dc2a9bf341c88f06b45e70f90059e3854a
Leo Deng <myst.dg@gmail.com>
2023-02-15 18:10:30 +0800

    djam

---
+++ keyboards/dg/djam/mega32u4/config.h
@@ -0,0 +1,11 @@
+/* ATMEGA32U4 */
+
+/* Matrix */
+#define DIODE_DIRECTION COL2ROW
+#define MATRIX_ROW_PINS { F0, F1, F4 }
+#define MATRIX_COL_PINS { C7, C6, B6, B5, B4, D7, D6, D4 }
+
+/* RGB Matrix */
+#ifdef RGB_MATRIX_ENABLE
+#define WS2812_DI_PIN D5
+#endif
