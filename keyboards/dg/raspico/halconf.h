4bbd2f823e3073812862e6c66ff4650e82a36181
Leo Deng <myst.dg@gmail.com>
2023-02-15 18:10:49 +0800

    raspico

---
+++ keyboards/dg/raspico/halconf.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include_next <halconf.h>
+
+#undef HAL_USE_I2C
+#define HAL_USE_I2C TRUE
+
+#undef HAL_USE_SPI
+#define HAL_USE_SPI TRUE
+
+#undef SPI_USE_WAIT
+#define SPI_USE_WAIT TRUE
+
+#undef SPI_SELECT_MODE
+#define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
