fb2c9bd613b363252223c3ba8a6ad2e6a0d61339
Leo Deng <myst.dg@gmail.com>
2024-05-23 11:13:43 +0800

    djam

---
+++ keyboards/dg/djam/rev1/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* Rev 1: STM32F401 */
+
+#define BOOTMAGIC_LITE_ROW 1
+#define BOOTMAGIC_LITE_COLUMN 0
+#define DEBOUNCE 3
+#define MATRIX_IO_DELAY 1
+#define USB_POLLING_INTERVAL_MS 1
