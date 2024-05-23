d5e7a5f89603a40607122e6999f49e1e0460ed85
Leo Deng <myst.dg@gmail.com>
2024-05-23 11:13:43 +0800

    djam

---
+++ keyboards/dg/djam/rev2/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* Rev 2: STM32F401 Optical Switch */
+
+#define BOOTMAGIC_LITE_ROW 2
+#define BOOTMAGIC_LITE_COLUMN 7
+#define DEBOUNCE 1
+#define MATRIX_IO_DELAY 1
+#define USB_POLLING_INTERVAL_MS 1
