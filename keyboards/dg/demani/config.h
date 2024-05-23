76117411d4af1068754751cfc1900133c4119e19
Leo Deng <myst.dg@gmail.com>
2024-05-23 11:15:08 +0800

    demani

---
+++ keyboards/dg/demani/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/* Matrix */
+#define DEBOUNCE 1
+#define MATRIX_IO_DELAY 1
+#define USB_POLLING_INTERVAL_MS 1
+
+/* Encoder */
+#define ENCODERS_PAD_A { A0 }
+#define ENCODERS_PAD_B { A1 }
+#define ENCODER_RESOLUTION 10
+// #define ENCODER_RESOLUTIONS { 10, 12 }
